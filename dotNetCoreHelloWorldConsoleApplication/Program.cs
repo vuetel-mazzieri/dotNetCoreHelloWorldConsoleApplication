7c589dd0a177ba170322702db8ad0d34e4e45213
Alessio Mazzieri <alessio.mazzieri@vuetel.com>
2018-06-06 08:49:22 +0200

    Add project files.

---
+++ dotNetCoreHelloWorldConsoleApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotNetCoreHelloWorldConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
