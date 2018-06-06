f9aed98cfe5e69f23fa6c7f6f79966425dfbe11e
Alessio Mazzieri <alessio.mazzieri@vuetel.com>
2018-06-06 09:04:56 +0200

    Modified base text.

--- dotNetCoreHelloWorldConsoleApplication/Program.cs
+++ dotNetCoreHelloWorldConsoleApplication/Program.cs
@@ -6,7 +6,7 @@ namespace dotNetCoreHelloWorldConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World first console app");
             Console.ReadLine();
         }
     }
