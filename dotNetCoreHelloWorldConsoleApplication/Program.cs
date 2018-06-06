17eb6e57353d18b706fe7c15fcf8b70bbfafa4de
Alessio Mazzieri <alessio.mazzieri@vuetel.com>
2018-06-06 08:57:06 +0200

    edited Program.cs to add final ReadLine() method to Console

--- dotNetCoreHelloWorldConsoleApplication/Program.cs
+++ dotNetCoreHelloWorldConsoleApplication/Program.cs
@@ -7,6 +7,7 @@ namespace dotNetCoreHelloWorldConsoleApplication
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
